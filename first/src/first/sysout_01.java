e2c09c953bc2808d0e889ce124d77010f49ba17c
Mailro <kawea3@naver.com>
2021-06-24 22:06:45 +0900

    백준 고양이 출력 문제 \ 연습

---
+++ first/src/first/sysout_01.java
@@ -0,0 +1,13 @@
+package first;
+
+public class sysout_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\ \t /\\");
+        System.out.println(" )   ( ')");
+        System.out.println(")\t/\t)");
+        System.out.println(" \\(__)|");
+	}
+
+}
