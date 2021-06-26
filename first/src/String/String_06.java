afd5ce6db48390906f2301851c6e43e164899939
Mailro <kawea3@naver.com>
2021-06-26 23:54:58 +0900

    자바 백준 1152번 단어 갯수 구하기(1)차

---
+++ first/src/String/String_06.java
@@ -0,0 +1,20 @@
+package String;
+
+import java.util.Scanner;
+
+public class String_06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		String str = scn.nextLine();
+		int cnt = 0;
+		for(int i = 0; i<str.length(); i++) {
+			if(str.charAt(i) == ' ') {
+				cnt++;
+			}
+		}
+		System.out.print(cnt);
+	}
+
+}
