2efe5c99b28a947aa07fb99549588fecc91172d1
Mailro <kawea3@naver.com>
2021-07-07 23:53:12 +0900

    자바 백준 기본 수학1

---
+++ first/src/math01/Math_01.java
@@ -0,0 +1,29 @@
+package math01;
+
+import java.util.Scanner;
+
+public class Math_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			
+				Scanner in = new Scanner(System.in);
+		 
+		 
+				int A = in.nextInt();	// 불변 비용
+				int B = in.nextInt();	// 가변 비용
+				int C = in.nextInt(); 	// 상품 가격
+		        
+				if (C <= B) {
+					System.out.println("-1");
+				} 
+				else {
+					System.out.println((A/(C-B))+1);
+				}
+			}
+		}
+		 
+	
+
+
