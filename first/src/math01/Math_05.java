ace2cf371aee42c4aa2dfb3bb7d1c71cf4ffe5bc
Mailro <kawea3@naver.com>
2021-07-17 23:56:26 +0900

    백준 10250번 ACM호텔 풀이

---
+++ first/src/math01/Math_05.java
@@ -0,0 +1,41 @@
+package math01;
+
+import java.util.Scanner;
+
+public class Math_05 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int T = input.nextInt();
+		String[] result = new String[T];
+		for(int i = 0; i < T; i++) {
+			int H = input.nextInt();	// ³ôÀÌ
+			int W = input.nextInt();	// °¡·Î
+			int N = input.nextInt();	// N¹øÂ° ¼Õ´Ô
+			
+
+			int height = N % H;   // Ãþ
+			int width;            // È£
+			
+			result[i] = "";
+			if(height == 0) {
+				result[i] += H;
+				width = N / H;
+			}else {
+				result[i] += height;
+				width = N / H + 1;
+			}
+			
+			if(width < 10) {
+				result[i] += "0";
+			}
+			result[i] += width;
+		}
+		
+		for(int i = 0; i < T; i++) {
+			System.out.println(result[i]);
+		}
+		
+		input.close();
+	}
+}
